12daa4f5a576afb8d6bf0625bd4d4274a1ffd166
LoricAndre <loric.andre@pm.me>
2023-05-08 17:39:16 +0200

    cleanup

--- lua/plugins.bak/sniprun.lua
+++
@@ -1,6 +0,0 @@
-local nmap = require 'utils'.nmap
-local vmap = require 'utils'.vmap
-
-nmap { '<leader>r', require 'sniprun'.run }
-nmap { '<Esc>', '<CMD>noh<BAR>:SnipClose<CR>' }
-vmap { '<leader>r', function() require 'sniprun'.run('v') end }
