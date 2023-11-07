e96623982bec5e0d7b0f8e4d5eb6c7ac09c3206e
LoricAndre <loric.andre@pm.me>
2023-11-07 08:55:01 +0100

    add dap

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.dap.core" },
     -- import any extras modules here
     -- import/override with your plugins
     { import = "plugins" },
